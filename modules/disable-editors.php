4af4b10aeff0c7468c3b5a3a2a55c04939f74231
Valentino Cossar <valentinocossar@gmail.com>
2015-09-11 19:05:42 +0200

    removed previous function to hide editors because causing a problem with the wordpress update alerts

--- modules/disable-editors.php
+++ modules/disable-editors.php
@@ -3,11 +3,12 @@
 namespace Ensoul\Rankz\DisableEditors;
 
 
-// Remove editors from backend with disallow file mods constant
-function disallow_file_mods() {
-  define('DISALLOW_FILE_MODS',true);
+// Remove submenu editors for plugins and theme files
+function hide_editors() {
+  remove_action('admin_menu', '_add_themes_utility_last', 101);
+  remove_submenu_page('plugins.php', 'plugin-editor.php');
 }
-add_action('admin_init', __NAMESPACE__ . '\\disallow_file_mods');
+add_action('admin_menu', __NAMESPACE__ . '\\hide_editors');
 
 // Redirect any user trying to access editors page
 function editors_admin_menu_redirect() {
