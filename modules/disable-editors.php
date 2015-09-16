b1783ae896e83a511243069abb4af32a079a5306
Valentino Cossar <valentinocossar@gmail.com>
2015-09-17 01:29:20 +0200

    now uses the same disable editors function that use rubble (bedrock)

--- modules/disable-editors.php
+++ modules/disable-editors.php
@@ -3,18 +3,8 @@
 namespace Ensoul\Rankz\DisableEditors;
 
 
-// Remove submenu editors for plugins and theme files
-function hide_editors() {
-  remove_action('admin_menu', '_add_themes_utility_last', 101);
-  remove_submenu_page('plugins.php', 'plugin-editor.php');
+// Remove editors for plugins and theme files
+function remove_editors() {
+  define('DISALLOW_FILE_EDIT', true);
 }
-add_action('admin_menu', __NAMESPACE__ . '\\hide_editors');
-
-// Redirect any user trying to access editors page
-function editors_admin_menu_redirect() {
-  global $pagenow;
-  if (($pagenow === 'theme-editor.php') || ($pagenow === 'plugin-editor.php')) {
-    wp_redirect(admin_url()); exit;
-  }
-}
-add_action('admin_init', __NAMESPACE__ . '\\editors_admin_menu_redirect');
+add_action('admin_init', __NAMESPACE__ . '\\remove_editors');
